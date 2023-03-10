5e38effc82943f77aca7808b2c6e4f513dd6ac7c
valenca959 <rodrigovenedino@gmail.com>
2023-03-10 20:49:42 -0300

    resolução do exercício 003

---
+++ 003/exe003.py
@@ -0,0 +1,6 @@
+print('Desafio de matemática')
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+s = n1 + n2
+# print('A soma entre', n1, 'e', n2,'é igual a {}. Correto?'.format(s))
+print(f'A soma entre {n1} e {n2} é igual a {s}')
