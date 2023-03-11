6feb13e21ac3413163208aeb2c1103d53c76fe18
valenca959 <rodrigovenedino@gmail.com>
2023-03-10 21:04:24 -0300

    resolução do exercício 013

---
+++ 013/exe013.py
@@ -0,0 +1,3 @@
+salario = float(input('qual é o salário do Funcionario? R$'))
+aumento = (salario * 15) / 100
+print(f'Um funcionário que ganhava R${salario}, com 15% de aumento, passa a receber R${salario + aumento:.2f}')
