f73d9b2963ad178fec70c709d63d62e6790d19ad
valenca959 <rodrigovenedino@gmail.com>
2023-03-10 21:10:52 -0300

    resolução do exercício 037

---
+++ 037/exe037.py
@@ -0,0 +1,14 @@
+num = int(input("Digite um número inteiro: "))
+print('''Escolha uma das bases para conversão
+ [ 1 ] converter para BINÀRIO
+ [ 2 ] convertar para OCTAL
+ [ 3 ] converter para HEXADECIMAL''')
+opcao = int(input("Sua opção: "))
+if opcao == 1:
+    print(f"{num} convertido para BINÀRIO é igual a { bin(num)[2:] }")
+elif opcao == 2:
+    print(f"{num} convertido para OCTAL é igual a {oct(num)[2:]}")
+elif opcao == 3:
+    print(f"{num} convertido para HEXADECIMAL é igual a {hex(num)[2:]}")
+else:
+    print("Opção imválida. Tente novamente")
