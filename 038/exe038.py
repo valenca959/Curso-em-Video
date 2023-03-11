3e4f76f4ce7c3f4019636d02b9bdc1c5b41161eb
valenca959 <rodrigovenedino@gmail.com>
2023-03-10 21:10:56 -0300

    resolução do exercício 038

---
+++ 038/exe038.py
@@ -0,0 +1,8 @@
+primeiro = int(input("Primeiro número: "))
+segundo = int(input("Segundo número: "))
+if primeiro > segundo:
+    print("O PRIMEIRO valor é maior")
+elif segundo > primeiro:
+    print("O segundo valor é maior")
+else:
+    print("Não existe valor maior, os dois são iguais")
