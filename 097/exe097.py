429c16b534693530cf91192a4ef7eb904f7aa666
valenca959 <rodrigovenedino@gmail.com>
2023-03-11 22:31:14 -0300

    resolução do exercício 097

---
+++ 097/exe097.py
@@ -0,0 +1,10 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print("-" * tam)
+    print(f"  {msg}")
+    print("-" * tam)
+
+
+# Programa Principal
+escreva('Curso em Video')
+escreva('EXERCÌCIO PYTHON 097')
