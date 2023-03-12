58b64508134f71d3af1f8bd6f71c24c6fe099b13
valenca959 <rodrigovenedino@gmail.com>
2023-03-11 22:28:51 -0300

    resolução do exercício 075

---
+++ 075/exe075.py
@@ -0,0 +1,13 @@
+numeros = (int(input("Digite um número: ")), int(input("Digite outro número: ")),
+int(input("Digite mais um número: ")), int(input("Digite o último número: ")))
+print(f"Você digitou os valores {numeros}")
+print(f"O valor 9 apareceu {numeros.count(9)} vezes")
+if 3 in numeros:
+    print(f"o valor 3 apareceu na {numeros.index(3)+1}° posição")
+else:
+    print("O valor 3 não foi digitado em nenhuma posição")
+print("Os valores pares digitados foram", end=' ')
+for n in numeros:
+    if n % 2 == 0:
+        print(n,end=' ')
+        
